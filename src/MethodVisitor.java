c1e5161126882805a436b475a9641f048eb6123d
Claire Higgins <claireeh2000@googlemail.com>
2021-10-14 11:26:22 +0100

    WCM2 fixed to account for try clause & RFC modified to match expected outputs for bank files.

--- src/MethodVisitor.java
+++ src/MethodVisitor.java
@@ -1,9 +1,10 @@
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.MethodDeclaration;
-import com.github.javaparser.ast.body.VariableDeclarator;
+import com.github.javaparser.ast.expr.ConditionalExpr;
+import com.github.javaparser.ast.expr.Expression;
 import com.github.javaparser.ast.expr.MethodCallExpr;
-import com.github.javaparser.ast.expr.VariableDeclarationExpr;
+import com.github.javaparser.ast.stmt.*;
 import com.github.javaparser.ast.visitor.VoidVisitor;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
@@ -14,47 +15,62 @@ import java.util.List;
 
 public class MethodVisitor {
 
-    private static final String FILE_PATH = "./Files/Simulator.java";
+    private static final String FILE_PATH = "./Files/Animal.java";
 
     public static void main(String[] args) throws Exception {
         File dir = new File("./Files");
         File[] listFiles = dir.listFiles();
+
+
+        System.out.format("%s %32s %16s %16s %16s %16s\n", "Class", "WMC", "WMC(Complex)", "RFC", "CBO", "LCOM");
+
+        // File file = new File("./Files/Field.java");
         for (File file : listFiles) {
             CompilationUnit cu = StaticJavaParser.parse(new FileInputStream(file.getPath()));
 
-            //VoidVisitor<Void> methodNameVisitor = new MethodNamePrinter();
-            //methodNameVisitor.visit(cu, null);
-           // List<String> methodNames = new ArrayList<>();
-           // VoidVisitor<List<String>> methodNameCollector = new MethodNameCollector();
-           // methodNameCollector.visit(cu, methodNames);
+
+            System.out.print(file.getPath());
+            //WMC 1 - SIMPLE
+
+            List<String> methodNames = new ArrayList<>();
+            VoidVisitor<List<String>> methodNameCollector = new MethodNameCollector();
+            methodNameCollector.visit(cu, methodNames);
             //methodNames.forEach(n -> System.out.println("Method Name Collected: " + n));
             //System.out.println("No. of methods in " + file.getPath() + ": " + methodNames.size());
 
-            //List<String> methodCalls = new ArrayList<>();
-            //VoidVisitor<List<String>> methodCallCollector = new MethodCallCollector();
-            //methodCallCollector.visit(cu, methodCalls);
-            //methodCalls.forEach(n -> System.out.println("Method Call Collected: " + n));
+            System.out.format("%16d",methodNames.size());
+
+            //WMC 2 - WEIGHTED
+
+            List<String> methodDecisions = new ArrayList<>();
+            VoidVisitor<List<String>> wmc2Metric = new WMC2();
+            wmc2Metric.visit(cu, methodDecisions);
+
+            System.out.format("%16d",  methodDecisions.size());
+            //System.out.println("WMC count for " + file.getPath() + ": " + methodDecisions.size());
+
+
+            //RFC
+            List<String> methodCalls = new ArrayList<>();
+            VoidVisitor<List<String>> methodCallCollector = new MethodCallCollector();
+            methodCallCollector.visit(cu, methodCalls);
+            System.out.format("%16d", methodCalls.size());
+
+            // methodCalls.forEach(n -> System.out.println("Method Call Collected: " + n));
             //System.out.println("No. of method calls in " + file.getPath() + ": " + methodCalls.size());
+            //   }
 
+            //CBO
             List<String> coupledClasses = new ArrayList<>();
-            VoidVisitor<List<String>> classRefCollector = new ClassRefCollector();
-            classRefCollector.visit(cu, coupledClasses);
+            VoidVisitor<List<String>> cBOCalculator = new CBOCalculator();
+            cBOCalculator.visit(cu, coupledClasses);
+            System.out.format("%16s", "-" );
 
-            coupledClasses.forEach(n -> System.out.println("Coupling detected with class: " + n));
-
-
+            System.out.format("%16s \n", "-" );
         }
     }
 
-    private static class MethodNamePrinter extends VoidVisitorAdapter<Void> {
-
-        @Override
-        public void visit(MethodDeclaration md, Void arg) {
-            super.visit(md, arg);
-            System.out.println("Method Name Printed: " + md.getName());
-        }
-    }
-
+    //WMC 1 - SIMPLE
     private static class MethodNameCollector extends VoidVisitorAdapter<List<String>> {
 
         @Override
@@ -64,31 +80,160 @@ public class MethodVisitor {
         }
     }
 
+    //WMC 2 - WEIGHTED
+
+    private static class WMC2 extends VoidVisitorAdapter<List<String>>{
+
+        //gets method declaration
+        public void visit(MethodDeclaration md, List<String> collector){
+            super.visit(md, collector );
+            collector.add(md.getNameAsString());
+        }
+
+        //honestly unsure what this does, dont think it does anything
+        public void visit(ConditionalExpr cExpr, List<String> collector){
+            super.visit(cExpr, collector);
+            collector.add(cExpr.toString());
+
+
+            Expression expr = cExpr.getCondition();
+            collector = this.checkExpression(cExpr.getCondition(), collector);
+
+        }
+
+        //checks for loops and adds to list
+        public void visit(ForStmt stmt, List<String> collector){
+            super.visit(stmt, collector);
+
+            Expression expr = stmt.asForStmt().getCompare().get();
+            collector = this.checkExpression(expr, collector);
+            collector.add(stmt.toString());
+        }
+
+        //checks for do loops and adds to list
+        public void visit(DoStmt stmt, List<String> collector){
+            super.visit(stmt, collector);
+            collector.add(stmt.toString());
+
+            collector = this.checkExpression(stmt.getCondition(), collector);
+
+        }
+
+        //checks for each
+        public void visit(ForEachStmt stmt, List<String> collector){
+            super.visit(stmt, collector);
+            collector.add(stmt.toString());
+        }
+
+        //checks for ifs
+        public void visit(IfStmt stmt, List<String> collector){
+            super.visit(stmt, collector);
+            collector.add(stmt.toString());
+
+            //checking for ands / or ors
+
+            //gets condition
+            Expression expr = stmt.getCondition();
+
+            collector = this.checkExpression(stmt.getCondition(), collector);
+        }
+
+        //checks for switches
+        public void visit(SwitchStmt stmt, List<String> collector){
+            super.visit(stmt, collector);
+            collector.add(stmt.toString());
+        }
+
+        //checks for while
+        public void visit(WhileStmt stmt, List<String> collector){
+            super.visit(stmt, collector);
+            collector.add(stmt.toString());
+
+            collector = this.checkExpression(stmt.getCondition(), collector);
+        }
+
+        //checks try statements
+
+        public void visit(TryStmt stmt, List<String> collector){
+            super.visit(stmt, collector);
+            collector.add(stmt.toString());
+
+
+
+        }
+
+        public List<String> checkExpression(Expression expr, List<String> collector){
+            if(expr.isBinaryExpr()){
+                if(expr.asBinaryExpr().getOperator().asString() == "||" || expr.asBinaryExpr().getOperator().asString() == "&&"){
+                    collector.add(expr.toString());
+                }
+                if (expr.asBinaryExpr().getLeft().isBinaryExpr()){
+                    collector = checkExpression(expr.asBinaryExpr().getLeft(), collector);
+                }
+                if(expr.asBinaryExpr().getRight().isBinaryExpr()){
+                    collector = checkExpression(expr.asBinaryExpr().getLeft(), collector);
+                } else if (expr.asBinaryExpr().getRight().isEnclosedExpr()){
+                    expr = expr.asBinaryExpr().getRight().asEnclosedExpr().getInner();
+                    collector = checkExpression(expr, collector);
+                }
+
+            }
+
+            return collector;
+        }
+
+
+
+    }
+
+    //RFC
     private static class MethodCallCollector extends VoidVisitorAdapter<List<String>> {
 
         @Override
         public void visit(MethodCallExpr mce, List<String> collector) {
             super.visit(mce, collector);
-            collector.add(mce.getNameAsString());
-        }
-    }
 
-    private static class ClassRefCollector extends VoidVisitorAdapter<List<String>> {
-
-        //used to check method declarations
-
-
-        //used to check for variables
-        public void visit(VariableDeclarationExpr n, List<String> collector){
-            for (VariableDeclarator var : n.getVariables()){
-
-                if(!collector.contains(var.getTypeAsString())){
-                    collector.add(var.getTypeAsString());
-                }
+            if(!collector.contains(mce.getNameAsString())){
+                collector.add(mce.getNameAsString());
             }
-            super.visit(n, collector);
+
+        }
+
+        @Override
+        public void visit(MethodDeclaration md, List<String> collector) {
+            super.visit(md, collector);
+
+            if(!collector.contains(md.getNameAsString())){
+                collector.add(md.getNameAsString());
+            }
+
         }
 
     }
+
+    //CBO
+
+    private static class CBOCalculator extends VoidVisitorAdapter<List<String>> {
+
+        @Override
+        public void visit(MethodCallExpr mce, List<String> collector) {
+            super.visit(mce, collector);
+
+
+            if(mce.getScope().isPresent()){
+               // System.out.println(mce.toString());
+                Expression expr = mce.getScope().get();
+
+                if(expr.isNameExpr()){
+                    expr.asNameExpr().getClass();
+                }
+
+            }
+        }
+
+
+
+    }
+
 
 }
